71e3673a7df269d48540aa1fbd4c9d07a5ca4d62
patrickgrey <websemantic@yahoo.co.uk>
2022-02-23 13:41:25 +0000

    Using subdirectories not working

---
+++ src/1.0-the-training-organisation/3.1-preparing-to-deliver-an-ojt-session/main.js
@@ -0,0 +1,10 @@
+var pageModule = (function () {
+  var module = {};
+  module.init = function () {
+    // ScrollToTop();
+    console.log("hi");
+  };
+  return module;
+})();
+
+pageModule.init();
